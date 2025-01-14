720b2884be2d0b60d8fa3fdb5caace03e7831a1f
unknown <dinoz64000@gmail.com>
2025-01-14 15:01:15 +0100

    Mise en place accés db
    Acces a un container docker avec Mysql Port : 3306
    Nom de la db : Breizhsport

--- db.config.js
+++ db.config.js
@@ -6,11 +6,11 @@ const { Sequelize } = require('sequelize')
 /*** Connexion à la base de données */
 let sequelize = new Sequelize(
     process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql',
-        logging: false
-    }
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: true
+}
 )
 
 /*** Mise en place des relations */
@@ -20,8 +20,8 @@ db.sequelize = sequelize
 db.User = require('./models/user')(sequelize)
 db.Cocktail = require('./models/cocktail')(sequelize)
 
-db.User.hasMany(db.Cocktail, {foreignKey: 'user_id', onDelete: 'cascade'})
-db.Cocktail.belongsTo(db.User, {foreignKey: 'user_id'})
+db.User.hasMany(db.Cocktail, { foreignKey: 'user_id', onDelete: 'cascade' })
+db.Cocktail.belongsTo(db.User, { foreignKey: 'user_id' })
 
 
 /*********************************/
@@ -29,6 +29,6 @@ db.Cocktail.belongsTo(db.User, {foreignKey: 'user_id'})
 // sequelize.sync(err => {
 //     console.log('Database Sync Error', err)
 // })
-db.sequelize.sync({alter: true})
+// db.sequelize.sync({ alter: true })
 
 module.exports = db
